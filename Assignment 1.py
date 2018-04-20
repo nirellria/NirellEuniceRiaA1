9f611b0cea8f578bccbfd448de7ac26b4eb68409
nirellria <nirelleunice.ria@my.jcu.edu.au>
2018-04-20 12:23:25 +0800

    add main function

--- Assignment 1.py
+++ Assignment 1.py
@@ -5,3 +5,24 @@ Nirell Eunice Ria
 from operator import itemgetter
 data_list=[]
 songs_file = "songs.csv"
+def main():
+    read_file()#reads songs.csv file
+    print("Songs To Learn 1.0 - by Nirell Eunice Ria")
+    number_songs = count_songs()  # counts how many songs in the csv file
+    print("{} songs loaded".format(number_songs))
+    while True:
+        print("Menu :")
+        print("L - List songs\nA - Add new song\nC - Complete a song\nQ - Quit")
+        answer = input(">>>")
+        answer = answer.upper()#transform all letters to be uppercase so the menu can handle uppercase and lowercase
+        answer = valid_answer(answer)#checks the validity of the input
+        if answer == "L":
+            list_song()
+        if answer == "C":
+            complete_song()
+        if answer == "A":
+            add_song()
+        elif answer == "Q":
+            quit_program()
+            break
+    print("Thank you, bye !")
